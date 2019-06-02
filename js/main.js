5a83c8bdd17c5827c01d72e403daf4facef9e9d2
khs6226 <khsun6226@naver.com>
2019-06-01 17:02:41 -0700

    added grid to articles section

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(function() {
             url: `https://api.nytimes.com/svc/topstories/v2/${$section}.json?api-key=uLQ2Nlh2HBI6Z7CqiGTRYcG2SUqOC6IN`
         }).done(function(data) {
             console.log(data);
+            $('.articles').append(`<li><a href = "${data.results[1].url}"><div class="image" style="background-image:url(${data.results[1].multimedia[4].url})"></div></a></li>`);
         });
     });
 })
