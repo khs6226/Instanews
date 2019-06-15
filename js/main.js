39cdd641aa6649b8519bea9a91e373b01f313086
khs6226 <khsun6226@naver.com>
2019-06-15 16:46:00 -0700

    JavaScript fully working with adding and replacing article items

--- js/main.js
+++ js/main.js
@@ -6,8 +6,12 @@ $(function() {
             dataType: 'json',
             url: `https://api.nytimes.com/svc/topstories/v2/${$section}.json?api-key=uLQ2Nlh2HBI6Z7CqiGTRYcG2SUqOC6IN`
         }).done(function(data) {
-            console.log(data);
-            $('.articles').append(`<li><a href = "${data.results[1].url}"><div class="image" style="background-image:url(${data.results[1].multimedia[4].url})"></div></a></li>`);
+            let news = data.results;
+            let select = news.filter(function(articles) {return articles.multimedia.length !==0}).slice(0,12);
+            $('li:nth-child(n-13)').remove();
+            select.forEach(function(story, index) {
+                $('.articles').append(`<li><a href = "${data.results[index].url}"><div class="image" style="background-image:url(${data.results[index].multimedia[4].url})"></div></a></li>`);
+            });
         });
     });
 })
