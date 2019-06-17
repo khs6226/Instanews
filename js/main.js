a9120f9a9d2e7ae84bbcaccbf9897616e8465d36
khs6226 <khsun6226@naver.com>
2019-06-17 00:16:01 -0700

    ajax error is now fixed and header work is done

--- js/main.js
+++ js/main.js
@@ -6,11 +6,17 @@ $(function() {
             dataType: 'json',
             url: `https://api.nytimes.com/svc/topstories/v2/${$section}.json?api-key=uLQ2Nlh2HBI6Z7CqiGTRYcG2SUqOC6IN`
         }).done(function(data) {
-            let news = data.results;
-            let select = news.filter(function(articles) {return articles.multimedia.length !==0}).slice(0,12);
+            let $news = data.results;
+            let $select = $news.filter(function(articles) {
+                let $images = articles.multimedia.length;
+                if ($images !== 0) {
+                    let $a = $images;
+                    return $a}
+                }).slice(0,12);
+            console.log($news);
             $('li:nth-child(n-13)').remove();
-            select.forEach(function(story, index) {
-                $('.articles').append(`<li><a href = "${data.results[index].url}"><div class="image" style="background-image:url(${data.results[index].multimedia[4].url})"></div></a></li>`);
+            $select.forEach(function(story) {
+                $('.articles').append(`<li><a href = "${story.url}"><div class="image" style="background-image:url(${story.multimedia[4].url})"></div></a></li>`);
             });
         });
     });
